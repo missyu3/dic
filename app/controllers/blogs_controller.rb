3bfcb46cb5035cb649b6d5b58c9cfc65d3b214e9
gameplan1975 <gameplan1975@gmail.com>
2020-04-27 08:11:03 +0000

    blog修正、blog削除追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,17 +1,24 @@
 class BlogsController < ApplicationController
-    def show
-        @blog = Blog.find_by(id: params[:id])
-        @user = User.find_by(id: @blog.user_id)
+  def show
+      @blog = Blog.find_by(id: params[:id])
+  end
+  def new
+    @blog = Blog.new
+  end
+  def update
+    if @blog.update(blog_params)
+      redirect_to @blog, notice: 'ブログを編集しました.'
+    else
+      render :edit
     end
-    def new
-        @blog = Blog.new
-    end
-    def update
-        if @blog.update(blog_params)
-          redirect_to @blog, notice: 'ブログを編集しました.'
-        else
-          render :edit
-        end
-      end
+  end
+  def destroy
+    @blog.destroy
+    redirect_to blogs_url, notice: '投稿を削除しました'
+  end
 
+  private
+  def blog_params
+    params.require(:blog).permit(:content)
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user_path(user.id)
+    else
+      render :new
+    end
+  end
+  def destroy
+    session.delete(:user_id)
+    redirect_to new_session_path
+  end
+end
