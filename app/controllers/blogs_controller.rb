506506e5d67e29f36b7012442067034e4ea4b46e
gameplan1975 <gameplan1975@gmail.com>
2020-04-27 07:48:11 +0000

    blog新規作成と詳細

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,17 @@
 class BlogsController < ApplicationController
+    def show
+        @blog = Blog.find_by(id: params[:id])
+        @user = User.find_by(id: @blog.user_id)
+    end
+    def new
+        @blog = Blog.new
+    end
+    def update
+        if @blog.update(blog_params)
+          redirect_to @blog, notice: 'ブログを編集しました.'
+        else
+          render :edit
+        end
+      end
+
 end
