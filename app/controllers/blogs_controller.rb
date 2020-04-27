46a53f418b0e18a03e57e351562c759757662a2e
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-27 20:39:15 +0900

    fix BlogControllerで宣言されていないmethodを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,24 +1,12 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :destroy]
-  def index
-    @blogs = Blog.all
-  end
-
-  def show
-  end
-
-  def destroy
-    @blog.destroy
-    redirect_to blogs_path, notice:"ブログを削除しました！"
-  end
+  before_action :set_params, only: [:edit, :show, :destroy]
 
   def new
     @blog = Blog.new
   end
-  def edit
-    @blog = Blog.find(params[:id])
-  end
-  
+
+  def edit; end
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -35,9 +23,24 @@ class BlogsController < ApplicationController
       render :edit
     end
   end
-  
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show; end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_params
+    @blog = Blog.find(params[:id])
   end
 end
