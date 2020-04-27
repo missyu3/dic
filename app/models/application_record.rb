22dc88470bda5c8ec75641930ae9ba3cf6c02b74
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-04-27 22:46:24 +0900

    Add 言語化対応

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,25 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def my_class
+    puts self.class
+  end
 end
+
+
+--cmand
+
+
+def call_my_class(instance)
+  puts instance.my_class
+end
+
+def call_my_name(instance)
+  puts instance.my_class
+end
+
+user = User.new
+blog = Blog.new
+
+call_my_class(user)
+call_my_class(blog)
