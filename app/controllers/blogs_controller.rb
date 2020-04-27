91f50eeb948099bc40e57398a741b5160984d9f4
gameplan1975 <gameplan1975@gmail.com>
2020-04-27 08:47:45 +0000

    blog新規、編集機能

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,18 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+  def index
+  end
+  def edit
+  end
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_url, notice: 'ブログを投稿しました'
+    else
+      render :new
+    end
+  end
   def update
     if @blog.update(blog_params)
       redirect_to @blog, notice: 'ブログを編集しました.'
@@ -12,13 +24,8 @@ class BlogsController < ApplicationController
       render :edit
     end
   end
-  def destroy
-    @blog.destroy
-    redirect_to blogs_url, notice: '投稿を削除しました'
-  end
-
   private
   def blog_params
-    params.require(:blog).permit(:content)
+    params.require(:blog).permit(:content, :title)
   end
 end
