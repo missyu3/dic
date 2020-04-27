e6a116f2c5c87f0ebcc93d1b6d615f9f2474eb33
akihito kawasaki <kawasaki03040304@gmail.com>
2020-04-27 19:23:32 +0900

    index,show修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,19 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :destroy]
   def index
     @blogs = Blog.all
   end
 
   def show
-    @blog = Blog.find(params[:id])
+  end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
-  
+
 end
