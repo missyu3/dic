731526414ce71cb4532ab3a80b723665374f7569
gameplan1975 <gameplan1975@gmail.com>
2020-04-27 11:22:29 +0000

    コントローラーの修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,26 +2,31 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
   def edit
     @blog = Blog.find(params[:id])
   end
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
-      redirect_to blogs_url, notice: 'ブログを投稿しました'
+      redirect_to blogs_path, notice: 'ブログを投稿しました'
     else
       render :new
     end
   end
+
   def update
+    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
-      redirect_to @blog, notice: 'ブログを編集しました.'
+      redirect_to blogs_path, notice: "ブログを編集しました"
     else
       render :edit
     end
   end
+
   private
   def blog_params
-    params.require(:blog).permit(:content, :title)
+    params.require(:blog).permit(:content, :title, :id)
   end
 end
