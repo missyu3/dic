fbf9daa74b156e221f032c7cf8db65768a011a30
yodev21 <yodev2177@gmail.com>
2020-04-27 23:41:45 +0900

    Add jqueryでのボタン作成 close #26

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :blogs, dependent: :destroy
   has_secure_password
+
+  validates :password, presence: true,
+                       length: {minimum: 6}
 end
