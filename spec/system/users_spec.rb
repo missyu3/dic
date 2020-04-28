965813436bb92a26ca6164e3ce2c8031b580527a
yodev21 <yodev2177@gmail.com>
2020-04-28 09:01:59 +0900

    Remove 不要なコメント削除 またRspec導入

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  describe "サインイン機能" do
-    context "サインインした場合" do
+  describe "サインアップ機能" do
+    context "サインアップした場合" do
       it "ユーザー詳細画面に遷移すること" do
-        
+        fill_in "user_name", with: "test User1"
+        fill_in "user_email", with: "test1@example.com"
+        fill_in "user_password", with: "testtest"
+        fill_in "user_password_confirmation", with: "testtest"
       end
     end
   end
